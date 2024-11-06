76991186b48be3cdb49e63c23191e08d9f085599
z2zone <hjun1114@gmail.com>
2024-11-06 16:07:17 -0500

    adding save, add, delete tasks feature

--- popup/popup.js
+++ popup/popup.js
@@ -1,18 +1,54 @@
 
+let taskList = [];
 const addTaskButton = document.querySelector(".add-task-button");
 addTaskButton.addEventListener('click', () => addTask());
+
+chrome.storage.sync.get(['taskList'], (res) => {
+    console.log(res.taskList);
+});
+
+const saveTasks = () => {
+    chrome.storage.sync.set({
+        taskList: taskList
+    });
+}
+
+const reRender = () => {
+    const taskContainer = document.querySelector('.task-container');
+    taskContainer.innerHTML = '';
+    taskList.forEach(element => {
+        taskContainer.appendChild(element);
+    });
+}
+
+const deleteTask = (wrapperID) => {
+    taskList.forEach((element, index) => {
+        if(element.id === wrapperID){
+            taskList.splice(index, 1);
+        }
+    });
+    reRender();
+}
+
 const addTask = () => {
     const singleTaskWrapper = document.createElement('span');
+    const wrapperID = new Date().getTime().toString();
+    singleTaskWrapper.id = wrapperID;
+
     const taskInput = document.createElement('input');
-    taskInput.text = 'Type in your task';
+    taskInput.placeholder = 'Type in your task';
+    taskInput.value = '';
 
     const deleteButton = document.createElement('button');
     deleteButton.innerHTML = 'x';
     deleteButton.className = 'task-delete-button';
+    deleteButton.addEventListener('click', ()=>{
+        deleteTask(wrapperID);
+    })
 
     singleTaskWrapper.appendChild(taskInput);
     singleTaskWrapper.appendChild(deleteButton);
 
-    const taskContainer = document.querySelector('.task-container');
-    taskContainer.appendChild(singleTaskWrapper);
+    taskList.push(singleTaskWrapper);
+    reRender();
 }
